a39a0ecd7ffd409bad70b12ea0278ae266128962
Fryskin <gman358325@gmail.com>
2023-10-22 10:35:40 +0200

    New staff was added

--- config/settings.py
+++ config/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pogtgres',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'mysecretpassword',
-        'HOST': '127.0.0.1'
+        'HOST': 'db'
     }
 }
 
@@ -183,10 +183,10 @@ if CACHE_ENABLED:
     }
 
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 
 
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 CELERY_TIMEZONE = 'Australia/Tasmania'
 
